9900ca722f0907155d0f2aef158db5c1bc65c9ae
Prem-23 <71540670+Prem-23@users.noreply.github.com>
2021-02-15 20:33:30 +0530

    Update nav.js

--- nav.js
+++ nav.js
@@ -2,5 +2,5 @@ let toggleButton = document.querySelector('.toggle');
 let navBar = document.querySelector('.nav');
 
 toggleButton.addEventListener('click', () => {
-    navBar.classList.toggle('active')
+    navBar.classList.toggle('active1')
 })
