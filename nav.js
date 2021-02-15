243cf1f61b04e8f51b10cae5a4ced46e460e1ea2
Prem-23 <71540670+Prem-23@users.noreply.github.com>
2021-02-15 16:07:14 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,6 @@
+let toggleButton = document.querySelector('.toggle');
+let navBar = document.querySelector('.nav');
+
+toggleButton.addEventListener('click', () => {
+    navBar.classList.toggle('active')
+})
